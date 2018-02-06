26c7caf91d182c0d7ad2e70bc16d123987a2b4f3
wahidpasha <wahidgunit@gmail.com>
2018-02-06 17:19:48 +0530

    semi final 3

---
+++ app/src/main/java/com/zinnox/projects/followappui/Utility.java
@@ -0,0 +1,30 @@
+package com.zinnox.projects.followappui;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**ō
+ * Created by apple on 18/01/18.
+ */
+
+public final class Utility
+{
+    public static final  String FONT_NAME = "fonts/HelveticaNeue-Bold.ttf";
+
+    public static void applyFontForTextView(Context context, TextView view)
+    {
+        Typeface tf = Typeface.createFromAsset(context.getAssets(), FONT_NAME);
+
+        view.setTypeface(tf);
+    }
+
+    public static void applyFontForButton(Context context, Button view)
+    {
+        Typeface tf = Typeface.createFromAsset(context.getAssets(), FONT_NAME);
+
+        view.setTypeface(tf);
+    }
+
+}
